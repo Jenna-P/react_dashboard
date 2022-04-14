203a40aae9f8f572684626efda2abca119b9093c
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2022-04-14 18:05:30 +0200

    home css styles

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './navbar.scss'
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+       <div className="wrapper">
+         <div className="search">
+           <input type="text" placeholder="Search" />
+           <SearchOutlinedIcon className="icon" />
+         </div>
+         <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon" />
+            
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+            
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img src="https://cdn4.vectorstock.com/i/thumb-large/38/88/happy-girl-avatar-funny-child-profile-picture-vector-41133888.jpg" 
+                 alt="profileImg"
+                 className="avatar" />
+          </div>
+         </div>
+       </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart'
+import Featured from '../../components/featured/Featured'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Widget from '../../components/widget/Widget'
+import List from '../../components/table/List'
 import './home.scss'
 
 function Home() {
   return (
     <div className="home">
-      Home
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type={"user"} />
+          <Widget type={"order"} />
+          <Widget type={"earning"} />
+          <Widget type={"balance"} />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTile">Latest Transactions</div>
+          <List />
+        </div>
+      </div>
     </div>
   )
 }
